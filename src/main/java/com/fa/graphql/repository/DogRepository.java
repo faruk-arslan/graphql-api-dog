595c0a53163cf4d032300bb9a3292712d96780c0
Faruk Arslan <farukarslan011@gmail.com>
2021-07-21 13:39:23 +0300

    We don't need to implement service and controller for a GraphQL API. If you still want to have a REST API available, you can add those files to the application as well, and the same endpoints as before will be available.

--- src/main/java/com/fa/graphql/repository/DogRepository.java
+++ src/main/java/com/fa/graphql/repository/DogRepository.java
@@ -9,12 +9,5 @@ import java.util.List;
 
 @Repository
 public interface DogRepository extends CrudRepository<Dog,Long> {
-    @Query("select d.id, d.breed from Dog d where d.id=:id")
-    String findBreedById(Long id);
 
-    @Query("select d.id, d.breed from Dog d")
-    List<String> findAllBreed();
-
-    @Query("select d.id, d.name from Dog d")
-    List<String> findAllName();
 }
